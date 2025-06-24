5ce83087a5be8b72855389f34758d2dc96e98298
Tim Pomaville <tpomavil@icloud.com>
2025-06-24 09:59:31 -0700

    updating my website

--- src/js/script.js
+++ src/js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer", "Chemical Engineer", "DevOps Engineer", "Data Engineer"],
+		strings: ["Software Engineer", "Solution Architect", "Chemical Engineer", "DevOps Engineer", "Data Engineer"],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
